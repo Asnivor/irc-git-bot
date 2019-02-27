e5e93a2ee95c18bf7228b56b2988d4a9f04c4a64
Asnivor <coding@asnitech.co.uk>
2019-02-27 12:16:11 +0000

    some additional bot message commands

--- src/modules/ircbot.js
+++ src/modules/ircbot.js
@@ -56,72 +56,134 @@ client.addListener("kick", function(channel, person, by, message) {
 });
 
 // message listener
-client.addListener("message", function(nick, to, text, message) {
-    console.log(nick);
-    console.log(to);
-    console.log(text);
-    console.log(message);
+client.addListener("message", function(nick, target, text, message) {
 
-    for (channel of channels) {
-        if (channel == to) {
-            // message sent to this channel
-            ParseMsg(channel, text);
-            return;
+    if (target.startsWith("#")) {
+        // message was sent to a channel
+        for (channel of channels) {
+            if (channel == target) {
+                // message sent to this channel
+                MessageHandler(target, nick, text, message);
+                return;
+            }
         }
     }
-
-    // if we get this far, it is a private message from a user
-    ParseMsg(nick, text);
-
+    else {
+        // it is a private message from a user
+        MessageHandler(null, nick, text, message);
+    }
 });
 
-// message parsing
-const ParseMsg = async function(channel, msg) {
-    if (msg.toUpperCase().startsWith("!GITBOT ") || !channel.startsWith("#")) {
+const MessageHandler = async function(destination, fromNick, text, message) {
 
-        var cmd = msg.toUpperCase().replace("!GITBOT ", ""); // msg.substring(8);
-        logger.info("Received channel command: " + cmd);
+    var commandString = text;
+    var prefix = message["prefix"];
+    var isChannel = false;
 
-        switch (cmd.toUpperCase()) {
+    // default to sending back as PM
+    var actualDestination = fromNick;
 
-            case "VERSION":
-
-                var gioRoot = await ghapi.GitioLookup("https://github.com/Asnivor/irc-git-bot");
-                var gio = await ghapi.GitioLookup("https://github.com/Asnivor/irc-git-bot/commit/" + info.sha);
-                var vTitle = "irc-git-bot (by Asnivor) " + gioRoot;
-
-                if (info.Tag) {
-                    // tag exists for current commit
-                    var tag = info.Tag;
-                    var version = tag;
-                }
-                else if (info.lastTag) {
-                    // a previous tag exists
-                    var tag = info.lastTag;
-                    var commitsSince = info.commitsSinceLastTag;
-                    var version = tag + " (+" + commitsSince + " commits)";
-                }
-                else {
-                    var version = "v:" + info.abbreviatedSha;
-                }
-
-                if (channel.startsWith("#")) {
-                    // it is a channel
-                    var outString = vTitle + " - " + version + " - commit: " + gio;
-                    client.say(channel, outString);
-                }
-                else {
-                    // it is a user (private message)
-                    client.say(channel, "irc-git-bot (by Asnivor)");
-                    client.say(channel, "https://github.com/Asnivor/irc-git-bot");
-                    client.say(channel, "Version: " + version);
-                    client.say(channel, "Commit: " + gio);
-                }
-
-                break;
-
-        }
+    if (destination) {
+        // original message was sent to channel rather than directly to the bot via PRIVMSG
+        actualDestination = destination;
+        isChannel = true;
     }
+
+    if (text.toUpperCase().startsWith("!GITBOT ")) {
+        // strip !gitbot
+        commandString = text.substring(8);
+    }
+
+    // get initial command
+    var command = commandString.split(" ")[0];
+    // remove command from commandString
+    commandString = commandString.substring(command.length + 1);
+
+    if (isChannel && !text.toUpperCase().startsWith("!GITBOT ")){
+
+        // message to channel was not prepended with !gitbot
+        // ignore (overrides may come in future)
+        return;
+    }
+
+    //console.log(actualDestination);
+    //console.log(prefix);
+    //console.log(command);
+    //console.log(commandString);
+
+    switch (command.toUpperCase()) {
+
+        // Help interface
+        case "HELP":
+
+            logger.info("Command rcvd from " + fromNick + " - " + command + " " + commandString);
+
+            // always send in PM
+            actualDestination = fromNick;
+
+            if (commandString.toUpperCase().startsWith("VERSION")){
+                client.say(actualDestination, "VERSION: Displays irc bot version info");
+            }
+            else if (commandString.toUpperCase().startsWith("SEARCHHASH")) {
+                client.say(actualDestination, "SEARCHHASH: Returns a GitHub search page based on the supplied hash");
+            }
+            else {
+                client.say(actualDestination, "VERSION: Displays irc bot version info");
+                client.say(actualDestination, "SEARCHHASH: Returns a GitHub search page based on the supplied hash");
+            }
+
+            break;
+
+        // prints bot version info
+        case "VERSION":
+
+            logger.info("Command rcvd from " + fromNick + " - " + command + " " + commandString);
+
+            var gioRoot = await ghapi.GitioLookup("https://github.com/Asnivor/irc-git-bot");
+            var gio = await ghapi.GitioLookup("https://github.com/Asnivor/irc-git-bot/commit/" + info.sha);
+            var vTitle = "irc-git-bot (by Asnivor) " + gioRoot;
+
+            if (info.Tag) {
+                // tag exists for current commit
+                var tag = info.Tag;
+                var version = tag;
+            }
+            else if (info.lastTag) {
+                // a previous tag exists
+                var tag = info.lastTag;
+                var commitsSince = info.commitsSinceLastTag;
+                var version = tag + " (+" + commitsSince + " commits)";
+            }
+            else {
+                var version = "v:" + info.abbreviatedSha;
+            }
+
+            if (isChannel) {
+                // it is a channel
+                var outString = vTitle + " - " + version + " - commit: " + gio;
+                client.say(actualDestination, outString);
+            }
+            else {
+                // it is a user (private message)
+                client.say(actualDestination, "irc-git-bot (by Asnivor)");
+                client.say(actualDestination, "https://github.com/Asnivor/irc-git-bot");
+                client.say(actualDestination, "Version: " + version);
+                client.say(actualDestination, "Commit: " + gio);
+            }
+
+            break;
+
+        // link to github search page
+        case "SEARCHHASH":
+
+            logger.info("Command rcvd from " + fromNick + " - " + command + " " + commandString);
+
+            var searchPage = await ghapi.GitioLookup("https://github.com/search?q=hash%3A+" + commandString + "&type=Commits");
+            client.say(actualDestination, searchPage);
+
+            break;
+    }
+
 }
 
 // registered join
